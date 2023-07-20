27b567edd8de476da180cd40d433330df3455559
DevJhoan <znotjhoan@gmail.com>
2023-07-19 23:51:26 -0500

    fix: :bug: Optional types (command and events)

--- dist/structures/Addon.d.ts
+++ dist/structures/Addon.d.ts
@@ -2,18 +2,18 @@ import { ClientEvents } from "discord.js";
 import { Command } from "./Command";
 import { Event } from "./Event";
 interface AddonType {
-    name: string;
-    author: string;
-    commands: Command[];
-    events: Event<any>[];
-    version: string;
+	name: string;
+	author: string;
+	commands?: Command[];
+	events?: Event<keyof ClientEvents>[];
+	version: string;
 }
 declare class Addon {
-    name: string;
-    author: string;
-    commands?: Command[];
-    events?: Event<keyof ClientEvents>[];
-    version: string;
-    constructor(addon: AddonType);
+	name: string;
+	author: string;
+	commands?: Command[];
+	events?: Event<keyof ClientEvents>[];
+	version: string;
+	constructor(addon: AddonType);
 }
 export default Addon;
